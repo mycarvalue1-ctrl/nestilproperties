f3f61d5696faf3baefef93ec8391f0feda71ece9
Mycarvalue <mycarvalue1@gmail.com>
2026-02-15 08:41:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4501973510-5ada2",
+  "appId": "1:252611165002:web:7ec7629a8c4e84e9b99fb3",
+  "apiKey": "AIzaSyA8-9oTShBALCDtVh_ZmRSW1dtMNs-X9Uo",
+  "authDomain": "studio-4501973510-5ada2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "252611165002"
+};
