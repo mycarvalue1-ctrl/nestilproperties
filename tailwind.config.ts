c22453a8a0d0f6d74927f59814c955484d9feca0
Mycarvalue <mycarvalue1@gmail.com>
2026-03-09 04:53:43 +0000

    the animation is very slow before it is working perfectly can u please c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -105,7 +105,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'ticker': 'ticker 40s linear infinite',
+        'ticker': 'ticker 20s linear infinite',
         'blob': 'blob 8s ease-in-out infinite',
       },
     },
